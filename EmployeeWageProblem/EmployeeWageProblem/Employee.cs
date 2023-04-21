772378962ccff47692a5bb9858733de44cf39fdc
Rohan Shewale <shewalerohan50@gmail.com>
2023-04-21 08:47:48 +0530

    [Rohan]UC1: Check if employee is present or absent

---
+++ EmployeeWageProblem/EmployeeWageProblem/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class Employee
+    {
+        const int FULLTIME = 1;
+        public void CheckAttendence()
+        {
+            Random random = new Random();
+            int attendence = random.Next(0,2);
+            if (attendence == 1)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+        }
+    }
+}
---
+++ EmployeeWageProblem/EmployeeWageProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/-----Employee Wage Problem-----/");
+            Employee employee = new Employee();
+            employee.CheckAttendence();
+
+            Console.ReadKey();
+        }
+    }
+}
